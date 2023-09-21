4d6db97790ee55f9f8da42b1cf0a14ae44b34721
DmitryIll <dnillarionov@gmail.com>
2023-09-21 16:45:33 +0300

    +iac+secrets

---
+++ iac-terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+locals {
+  zone = "ru-central1-a"
+}
+
+provider "yandex" {
+  token = "y0_AgAAAAAEFd7ZAATuwQAAAADsuDodfIgSb4p9QBODVoXGdS6FclisQD4"
+  cloud_id = "b1gq7flktn6couidedto"
+  folder_id = "b1g6k2i3lobiesnh55af"
+  zone = local.zone
+}
+
+resource "yandex_compute_instance" "jenkins" {
+  name                      = "jenkins"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v1"
+  zone                      = local.zone
+
+  resources {
+    core_fraction = 5
+    cores  = "2"
+    memory = "2"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd891ub8nj4kg2g9o383"   #jenkins
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    nat       = true
+  }
+
+  scheduling_policy {
+  preemtible = true
+   }
+
+
+ metadata = {
+    user-data = "${file("./meta.yaml")}"
+  }
+
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+#  zone           = "<зона_доступности>"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = "${yandex_vpc_network.network-1.id}"
+}
+
+
